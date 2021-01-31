357f511733017bad9d50b942de2f79a90f292f2f
salmanlone2020 <63392556+salmanlone2020@users.noreply.github.com>
2021-01-31 13:00:02 +0800

    whole project

--- AlgorithmApi.LevenshteinDistance/LevenshteinDistanceService.cs
+++ AlgorithmApi.LevenshteinDistance/LevenshteinDistanceService.cs
@@ -15,15 +15,25 @@ namespace AlgorithmApi.LevenshteinDistance
 			return Compute(first, second);
 		}
 
-		private int Compute(string first,string second)
+		private int Compute(string first, string second)
 		{
-			if (string.IsNullOrEmpty(first))
-				return -1;
+			if (string.IsNullOrEmpty(first) && string.IsNullOrEmpty(second)) return -1;
 
-			if (string.IsNullOrEmpty(second))
-				return -1;
+			if (string.IsNullOrEmpty(first)) return second.Length;
 
+			if (string.IsNullOrEmpty(second)) return first.Length;
+
+			int[,] d = CreateMatrix(first, second);
+
+			FillUpMatrix(first, second, d);
+
+			return d[first.Length, second.Length];
+		}
+
+		private static int[,] CreateMatrix(string first, string second)
+		{
 			var d = new int[first.Length + 1, second.Length + 1];
+
 			for (var i = 0; i <= first.Length; i++)
 			{
 				d[i, 0] = i;
@@ -34,19 +44,21 @@ namespace AlgorithmApi.LevenshteinDistance
 				d[0, j] = j;
 			}
 
+			return d;
+		}
+
+		private void FillUpMatrix(string first, string second, int[,] d)
+		{
 			for (var i = 1; i <= first.Length; i++)
 			{
 				for (var j = 1; j <= second.Length; j++)
 				{
-					var cost = (second[j - 1] == first[i - 1]) ? 0 : 1;
-					d[i, j] = Min(
-						 d[i - 1, j] + 1,
-						 d[i, j - 1] + 1,
-						 d[i - 1, j - 1] + cost
-					);
+					if (second[j - 1] == first[i - 1])
+						d[i, j] = d[i - 1, j - 1];
+					else
+						d[i, j] = Min(d[i - 1, j], d[i, j - 1], d[i - 1, j - 1]) + 1;
 				}
 			}
-			return d[first.Length, second.Length];
 		}
 
 		private int Min(int e1, int e2, int e3)
