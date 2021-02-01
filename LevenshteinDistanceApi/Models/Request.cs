07f0733128afb3c3797028b2f54877ba670af2e1
salmanlone2020 <63392556+salmanlone2020@users.noreply.github.com>
2021-02-01 16:50:53 +0800

    validation on request model for max input length

--- LevenshteinDistanceApi/Models/Request.cs
+++ LevenshteinDistanceApi/Models/Request.cs
@@ -1,8 +1,12 @@
-﻿namespace AlgorithmApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AlgorithmApi.Models
 {
 	public class Request
 	{
+		[StringLength(50, ErrorMessage = "Name length can't be more than 50.")]
 		public string FirstInput { get; set; }
+		[StringLength(50, ErrorMessage = "Name length can't be more than 50.")]
 		public string SecondInput { get; set; }
 	}
 }
